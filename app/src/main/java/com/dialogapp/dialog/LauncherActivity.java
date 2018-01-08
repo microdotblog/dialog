776b166bd070231947a9301ed0d06991400cfcc8
rupak0577 <rupak0577@gmail.com>
2018-01-08 10:04:11 +0530

    Initial commit

---
+++ app/src/main/java/com/dialogapp/dialog/LauncherActivity.java
@@ -0,0 +1,12 @@
+package com.dialogapp.dialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
