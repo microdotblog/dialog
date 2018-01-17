d3be97cb0081e257534e8fc44ae8dd18ad0281e9
rupak0577 <rupak0577@gmail.com>
2018-01-17 12:18:11 +0530

    Add Account Dao and test

---
+++ app/src/main/java/com/dialogapp/dialog/db/AccountDao.java
@@ -0,0 +1,18 @@
+package com.dialogapp.dialog.db;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.dialogapp.dialog.model.AccountResponse;
+
+@Dao
+public interface AccountDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(AccountResponse accountResponse);
+
+    @Query("SELECT * FROM account WHERE username = :username")
+    LiveData<AccountResponse> fetchAccountInfo(String username);
+}
--- app/src/main/java/com/dialogapp/dialog/db/MicroBlogDb.java
+++ app/src/main/java/com/dialogapp/dialog/db/MicroBlogDb.java
@@ -3,6 +3,9 @@ package com.dialogapp.dialog.db;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-@Database(entities = {}, version = 1)
+import com.dialogapp.dialog.model.AccountResponse;
+
+@Database(entities = {AccountResponse.class}, version = 1)
 public abstract class MicroBlogDb extends RoomDatabase {
+    public abstract AccountDao accountDao();
 }
