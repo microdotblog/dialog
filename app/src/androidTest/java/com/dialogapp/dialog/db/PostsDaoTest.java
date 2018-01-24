709413b697534abdcedc5e79609d4bf429c47530
rupak0577 <rupak0577@gmail.com>
2018-01-25 02:40:42 +0530

    Add posts repository with test

--- app/src/androidTest/java/com/dialogapp/dialog/db/PostsDaoTest.java
+++ app/src/androidTest/java/com/dialogapp/dialog/db/PostsDaoTest.java
@@ -4,6 +4,7 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.dialogapp.dialog.TestUtil;
 import com.dialogapp.dialog.model.Item;
+import com.dialogapp.dialog.repository.Endpoints;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,11 +19,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class PostsDaoTest extends DbTest {
     @Test
     public void insertAndLoadTimeline() throws InterruptedException {
-        final List<Item> timelineItems = TestUtil.createListOfEndpoint("timeline");
+        final List<Item> timelineItems = TestUtil.createListOfEndpoint(Endpoints.TIMELINE);
         db.postsDao().insertTimeline(timelineItems);
 
-        final List<Item> loadedTimelineData = getValue(db.postsDao().loadEndpoint("timeline"));
-        assertThat(loadedTimelineData.get(8).getEndpoint(), is("timeline"));
+        final List<Item> loadedTimelineData = getValue(db.postsDao().loadEndpoint(Endpoints.TIMELINE));
+        assertThat(loadedTimelineData.get(8).getEndpoint(), is(Endpoints.TIMELINE));
         assertThat(loadedTimelineData.get(3).getId(), is(3L));
         assertThat(loadedTimelineData.get(7).getAuthor().getName(), is("foobar7"));
         assertThat(loadedTimelineData.get(7).getAuthor().getMicroblog().getUsername(), is("foo7"));
@@ -30,11 +31,11 @@ public class PostsDaoTest extends DbTest {
 
     @Test
     public void insertAndLoadMentions() throws InterruptedException {
-        final List<Item> mentionsItems = TestUtil.createListOfEndpoint("mentions");
+        final List<Item> mentionsItems = TestUtil.createListOfEndpoint(Endpoints.MENTIONS);
         db.postsDao().insertMentions(mentionsItems);
 
-        final List<Item> loadedMentionsData = getValue(db.postsDao().loadEndpoint("mentions"));
-        assertThat(loadedMentionsData.get(5).getEndpoint(), is("mentions"));
+        final List<Item> loadedMentionsData = getValue(db.postsDao().loadEndpoint(Endpoints.MENTIONS));
+        assertThat(loadedMentionsData.get(5).getEndpoint(), is(Endpoints.MENTIONS));
         assertThat(loadedMentionsData.get(21).getContentHtml(), is("blahblahblah21"));
         assertThat(loadedMentionsData.get(28).getAuthor().getUrl(), is("www.foo28.com"));
         assertThat(loadedMentionsData.get(27).getMicroblog().getIsDeletable(), is(false));
---
+++ app/src/main/java/com/dialogapp/dialog/repository/Endpoints.java
@@ -0,0 +1,10 @@
+package com.dialogapp.dialog.repository;
+
+/**
+ * Endpoint constants
+ */
+
+public final class Endpoints {
+    public static final String TIMELINE = "timeline";
+    public static final String MENTIONS = "mentions";
+}
