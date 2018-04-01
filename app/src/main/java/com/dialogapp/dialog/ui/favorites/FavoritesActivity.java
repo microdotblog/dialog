33e74ff585cc93430b151501a141bfc23c4e2064
Rupak <rupak0577@gmail.com>
2018-04-01 13:36:11 +0530

    Set FavoritesActivity's theme to NoActionBar

--- app/src/main/java/com/dialogapp/dialog/ui/favorites/FavoritesActivity.java
+++ app/src/main/java/com/dialogapp/dialog/ui/favorites/FavoritesActivity.java
@@ -3,6 +3,7 @@ package com.dialogapp.dialog.ui.favorites;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.dialogapp.dialog.R;
@@ -16,21 +17,25 @@ import butterknife.ButterKnife;
 public class FavoritesActivity extends BaseInjectableActivity implements BaseListFragment.FragmentEventListener {
     private Snackbar errorBar;
 
-    @BindView(R.id.coord_layout_common)
+    @BindView(R.id.toolbar_favorites)
+    Toolbar toolbar;
+
+    @BindView(R.id.coord_layout_favorites)
     CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_container);
+        setContentView(R.layout.activity_favorites);
         ButterKnife.bind(this);
 
         errorBar = Snackbar.make(coordinatorLayout, R.string.connection_error, Snackbar.LENGTH_LONG);
 
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container_common, ListFragment.newInstance(ListFragment.FAVORITES))
+                .replace(R.id.container_favorites, ListFragment.newInstance(ListFragment.FAVORITES))
                 .commit();
     }
 
