5d36c4dbdbde479f2c2e8b188b01ad3f8f186195
Rupak <rupak0577@gmail.com>
2018-05-03 10:17:39 +0530

    Add helper class to handle POST requests

---
+++ app/src/main/java/com/dialogapp/dialog/util/Event.java
@@ -0,0 +1,39 @@
+package com.dialogapp.dialog.util;
+
+import android.support.annotation.Nullable;
+
+public class Event<T> {
+
+    private final T content;
+    private boolean hasBeenHandled;
+
+    private Event(T content) {
+        this.content = content;
+    }
+
+    public static <T> Event<T> createEvent(T data) {
+        return new Event<>(data);
+    }
+
+    public boolean getHasBeenHandled() {
+        return hasBeenHandled;
+    }
+
+    @Nullable
+    public final T getContentIfNotHandled() {
+        if (hasBeenHandled)
+            return null;
+        else {
+            hasBeenHandled = true;
+            return content;
+        }
+    }
+
+    public T peekContent() {
+        return content;
+    }
+
+    private void setHasBeenHandled(boolean hasBeenHandled) {
+        this.hasBeenHandled = hasBeenHandled;
+    }
+}
