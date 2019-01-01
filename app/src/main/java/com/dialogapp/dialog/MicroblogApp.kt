c797f29378e6921f303809d4ba5363378ba2a271
Rupak <rupak0577@gmail.com>
2019-01-01 18:01:13 +0530

    Remove old shared prefs

--- app/src/main/java/com/dialogapp/dialog/MicroblogApp.kt
+++ app/src/main/java/com/dialogapp/dialog/MicroblogApp.kt
@@ -1,12 +1,14 @@
 package com.dialogapp.dialog
 
 import android.app.Application
+import android.content.Context
 import androidx.work.Configuration
 import androidx.work.WorkManager
 import com.dialogapp.dialog.di.AppComponent
 import com.dialogapp.dialog.di.DaggerAppComponent
 import com.squareup.leakcanary.LeakCanary
 import timber.log.Timber
+import java.io.File
 
 open class MicroblogApp : Application() {
 
@@ -35,7 +37,18 @@ open class MicroblogApp : Application() {
         INSTANCE = this
         appComponent = DaggerAppComponent.builder().application(this).build()
 
+        removeOldPrefs()
         configureWorkManager()
+    }
+
+    // TODO: remove
+    private fun removeOldPrefs() {
+        val file = File(filesDir.parent + File.separator + "shared_prefs" + File.separator +
+                "com.dialogapp.dialog.USER_PREFS.xml")
+        if (file.exists()) {
+            getSharedPreferences("com.dialogapp.dialog.USER_PREFS", Context.MODE_PRIVATE).edit().clear().commit()
+            file.delete()
+        }
     }
 
     private fun configureWorkManager() {
