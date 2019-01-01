96414cbbf84f7f035f002f78614ee70c844e56c4
Rupak <rupak0577@gmail.com>
2019-01-01 19:28:33 +0530

    Add Markdown options to toolbar

--- app/src/main/java/com/dialogapp/dialog/ui/posting/NewPostFragment.kt
+++ app/src/main/java/com/dialogapp/dialog/ui/posting/NewPostFragment.kt
@@ -1,5 +1,6 @@
 package com.dialogapp.dialog.ui.posting
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.text.Editable
@@ -9,6 +10,7 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
@@ -58,6 +60,7 @@ class NewPostFragment : Fragment(), OnBackPressedListener {
         super.onViewCreated(view, savedInstanceState)
 
         binding.bottomBarNewPost.inflateMenu(R.menu.bottom_bar_posting_menu)
+        setMenuItemOnClickListeners()
 
         val isReply = NewPostFragmentArgs.fromBundle(arguments!!).isReply
         val username: String
@@ -67,7 +70,7 @@ class NewPostFragment : Fragment(), OnBackPressedListener {
             postId = NewPostFragmentArgs.fromBundle(arguments!!).id
             username = NewPostFragmentArgs.fromBundle(arguments!!).username
             content = NewPostFragmentArgs.fromBundle(arguments!!).content
-            binding.editTextPost.setText("@$username ")
+            binding.editTextPost.append("@$username ")
             binding.bottomBarNewPost.menu.removeItem(R.id.posting_title)
             binding.bottomBarNewPost.menu.findItem(R.id.posting_parent_post).setOnMenuItemClickListener {
                 MaterialDialog(this.requireContext())
@@ -157,5 +160,49 @@ class NewPostFragment : Fragment(), OnBackPressedListener {
         } else {
             SpannableString(Html.fromHtml(html))
         }
+    }
+
+
+    private fun setMenuItemOnClickListeners() {
+        binding.bottomBarNewPost.menu.findItem(R.id.posting_markdown_bold).setOnMenuItemClickListener {
+            binding.editTextPost.text?.insert(binding.editTextPost.selectionStart, " **** ")
+            binding.editTextPost.setSelection(binding.editTextPost.selectionEnd - 3)
+            showKeyboard()
+            true
+        }
+
+        binding.bottomBarNewPost.menu.findItem(R.id.posting_markdown_italic).setOnMenuItemClickListener {
+            binding.editTextPost.text?.insert(binding.editTextPost.selectionStart, " __ ")
+            binding.editTextPost.setSelection(binding.editTextPost.selectionEnd - 2)
+            showKeyboard()
+            true
+        }
+
+        binding.bottomBarNewPost.menu.findItem(R.id.posting_markdown_link).setOnMenuItemClickListener {
+            MaterialDialog(this.requireContext()).show {
+                input(prefill = "https://") { _, text ->
+                    binding.editTextPost.text?.insert(binding.editTextPost.selectionStart, " []($text) ")
+                    binding.editTextPost.setSelection(binding.editTextPost.selectionEnd - (text.length + 4))
+                }
+                positiveButton {
+                    dismiss()
+                    binding.editTextPost.requestFocus()
+                    showKeyboard()
+                }
+                negativeButton()
+            }
+            true
+        }
+
+        binding.bottomBarNewPost.menu.findItem(R.id.posting_markdown_quote).setOnMenuItemClickListener {
+            binding.editTextPost.text?.insert(binding.editTextPost.selectionStart, "\n\n> ")
+            showKeyboard()
+            true
+        }
+    }
+
+    private fun showKeyboard() {
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
     }
 }
